67b35cc6b14ec248996c5a46cd563fe8c5941bb9
JC98683 <jcombs@msn.com>
2026-04-24 06:51:52 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,3 @@
-const CACHE = 'pdx-flight-entry-v2';
-const FILES = ['./', './index.html', './manifest.json', './icon.svg'];
-self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open(CACHE).then(function(cache){ return cache.addAll(FILES); }));
-  self.skipWaiting();
-});
-self.addEventListener('activate', function(event) {
-  event.waitUntil(caches.keys().then(function(keys){
-    return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
-  }));
-  self.clients.claim();
-});
-self.addEventListener('fetch', function(event) {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(function(cached){ return cached || fetch(event.request); }));
-});
+const CACHE='pdx-flight-entry-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
